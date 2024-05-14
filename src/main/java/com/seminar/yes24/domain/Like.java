0578ccbc3e9de54cb39b316ad2731d01f7bae68f
HongGit <soojam@konkuk.ac.kr>
2024-05-14 22:47:21 +0900

    Fix: member 오타 수정

--- src/main/java/com/seminar/yes24/domain/Like.java
+++ src/main/java/com/seminar/yes24/domain/Like.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class Like {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "memeber_runshow_like_id")
+    @Column(name = "member_runshow_like_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
