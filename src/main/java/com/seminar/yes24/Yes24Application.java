5fe9a20adceeb9812887ce5e730da17347ddbfec
paragon0107 <smg0725@naver.com>
2024-05-14 17:04:50 +0900

    start

---
+++ src/main/java/com/seminar/yes24/Yes24Application.java
@@ -0,0 +1,13 @@
+package com.seminar.yes24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yes24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yes24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/seminar/yes24/Yes24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seminar.yes24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yes24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
