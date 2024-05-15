75821e099e0f42725218d90a45808760b3fc9254
HongGit <soojam@konkuk.ac.kr>
2024-05-15 16:25:23 +0900

    Fix: api 버전 설정파일에서 관리

--- src/main/java/com/seminar/yes24/controller/RunShowController.java
+++ src/main/java/com/seminar/yes24/controller/RunShowController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
 public class RunShowController {
 
     private final RunShowService runShowService;
--- src/main/java/com/seminar/yes24/controller/ShowController.java
+++ src/main/java/com/seminar/yes24/controller/ShowController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
 public class ShowController {
     private final ShowService showService;
 
