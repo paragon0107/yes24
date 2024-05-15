44a2c4ccae75b18290386b58bc537f3bed75129e
HongGit <soojam@konkuk.ac.kr>
2024-05-15 17:01:28 +0900

    Feat: 상영정보 검색 컨트롤러 구현

--- src/main/java/com/seminar/yes24/controller/RunShowController.java
+++ src/main/java/com/seminar/yes24/controller/RunShowController.java
@@ -2,27 +2,26 @@ package com.seminar.yes24.controller;
 
 import com.seminar.yes24.dto.common.ResponseDto;
 import com.seminar.yes24.dto.response.RunShowFindDto;
+import com.seminar.yes24.dto.response.RunShowSearchDto;
 import com.seminar.yes24.service.RunShowService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.dialect.unique.CreateTableUniqueDelegate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class RunShowController {
-
     private final RunShowService runShowService;
+
     @GetMapping("/runshow/{runShowId}")
     public ResponseDto<RunShowFindDto> getRunShowDetail(
             @PathVariable Long runShowId
     ){
         return ResponseDto.ok(runShowService.findRunShowById(runShowId));
     }
+
     @PostMapping("/runshow/book/{runShowId}")
     public ResponseDto<Long> bookRunShow(
             @RequestHeader Long memberId,
@@ -31,5 +30,10 @@ public class RunShowController {
         return ResponseDto.created(runShowService.saveBookingById(memberId,runShowId));
     }
 
-
+    @GetMapping("/runshow/search")
+    public ResponseDto<List<RunShowSearchDto>> getRunShowSearch(
+            @RequestParam String query
+    ){
+        return ResponseDto.ok(runShowService.findRunShowByKeyWord(query));
+    }
 }
